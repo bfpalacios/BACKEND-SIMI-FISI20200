89563ed5d1d0826c06bb0f5fa65a40e02da175e3
wacarrear3128 <miguel.velasquez1@unmsm.edu.pe>
2020-02-26 03:47:12 -0500

    Tratamiento de datos duplicados

--- administrative-module/management-administrative-ws/src/main/java/pe/edu/ceid/simi/management/infraestructure/Tratamiento.java
+++ administrative-module/management-administrative-ws/src/main/java/pe/edu/ceid/simi/management/infraestructure/Tratamiento.java
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 package pe.edu.ceid.simi.management.infraestructure;
 
 import java.sql.Date;
@@ -43,43 +43,3 @@ public class Tratamiento {
 	}
 
 }
-=======
-package pe.edu.ceid.simi.management.infraestructure;
-
-import java.sql.Date;
-import java.util.Map;
-
-public class Tratamiento {
-
-	public static int tratarEntero(String columna, Map<String, Object> row) {
-		int ret = -1;
-		
-		try {
-			ret = Integer.parseInt(row.get(columna).toString());
-		} catch (Exception e) {}
-		
-		return ret;
-	}
-
-	public static String tratarString(String columna, Map<String, Object> row) {
-		String ret = "";
-		
-		try {
-			ret = row.get(columna).toString();
-		} catch (Exception e) {}
-		
-		return ret;
-	}
-
-	public static Date tratarDate(String columna, Map<String, Object> row) {
-		Date ret = null;
-		
-		try {
-			ret = (Date) row.get(columna);
-		} catch (Exception e) {}
-		
-		return ret;
-	}
-
-}
->>>>>>> branch 'dev' of https://github.com/bfpalacios/BACKEND-SIMI-FISI20200.git
