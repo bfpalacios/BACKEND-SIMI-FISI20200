7417ea9b0c839fbf222ba98eca6275a760cbe8ca
NatalyGVS <nataly.vasquez9718@gmail.com>
2020-02-27 09:55:51 -0500

    Modificacion URL Request

--- administrative-module/management-administrative-ws/src/main/java/pe/edu/ceid/simi/management/web/controllers/TipoEstudianteController.java
+++ administrative-module/management-administrative-ws/src/main/java/pe/edu/ceid/simi/management/web/controllers/TipoEstudianteController.java
@@ -18,7 +18,7 @@ import pe.edu.ceid.simi.management.domain.tipoestudiante.model.TipoEstudiante;
 
 @CrossOrigin(origins = "localhost:4200")
 @RestController
-@RequestMapping({"/api/v1/programacionDocente/tipoestudiante"})
+@RequestMapping({"/api/v1/procesomatricula/tipoestudiante"})
 public class TipoEstudianteController {
 	@Autowired
 	private TipoEstudianteService service;
